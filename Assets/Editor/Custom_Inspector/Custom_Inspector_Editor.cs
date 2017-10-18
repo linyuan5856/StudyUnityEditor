e03d5c44141be66fb3673a63bbd373ff8e2a3b1f
LinPc <linyuan5856139@163.com>
2017-10-18 15:13:40 +0800

    custom-inspector  Done

---
+++ Assets/Editor/Custom_Inspector/Custom_Inspector_Editor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(custom_inspector))]
+public class Custom_Inspector_Editor : Editor
+{
+    private custom_inspector inspector;
+
+
+    void OnEnable()
+    {
+        this.inspector = (custom_inspector) target;
+        inspector.name = "EnjoyCoding";
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Charactor InFo",GUILayout.MaxWidth(150));
+        inspector.name = EditorGUILayout.TextField("player Name", inspector.name);
+        EditorGUILayout.Space();
+
+        inspector.des = EditorGUILayout.TextArea("Actor Story",GUILayout.MinHeight(100));
+        EditorGUILayout.Space();
+
+        int[] data = new[] {10, 20, 30};
+        string[]show=new [] {"Min-10","Mid-20","Mix-30"};
+        inspector.age = EditorGUILayout.IntPopup("Age",inspector.age,show,data);
+        EditorGUILayout.Space();
+
+        inspector.atk = EditorGUILayout.Slider("Attack",inspector.atk, 0f, 10f);
+        EditorGUILayout.Space();
+
+        inspector.def = EditorGUILayout.FloatField("Defense", inspector.def);
+        EditorGUILayout.Space();
+
+        int max = 100;
+        inspector.hp = EditorGUILayout.IntSlider("Health", inspector.hp, 0,max); 
+        EditorGUILayout.Space();
+     
+               
+        Rect rect= EditorGUILayout.GetControlRect(false, 20);  
+        GUI.color = Color.green;
+        if (inspector.hp<80&&inspector.hp>50)
+        {
+            GUI.color = Color.yellow;
+        }
+        else if (inspector.hp<50)
+        {
+            GUI.color=Color.red;                  
+        }
+       
+        EditorGUI.ProgressBar(rect,(float)inspector.hp/max,"hp" );
+        EditorGUILayout.Space();
+        GUI.color = Color.black;
+        if (inspector.hp<20)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox("Hp is Low", MessageType.Warning);
+        }
+
+        EditorGUILayout.EndVertical();
+        
+    }
+}
---
+++ Assets/Script/Custom_Inspector/custom_inspector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class custom_inspector : MonoBehaviour
+{
+
+    public string name;
+    public int age;
+    public string address;
+    public int hp;
+    public float atk;
+    public float def;
+    public string des;
+}
