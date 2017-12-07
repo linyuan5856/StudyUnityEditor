f57eaaa7428d142e3a9493146995670b32960f9d
Lin--laptop <linyuan5856139@163.com>
2017-12-08 00:53:30 +0800

    Gizmos Editor

---
+++ Assets/Script/4.DrawGizmos/GizmosEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmosEditor : MonoBehaviour
+{
+
+    public Texture mTexture;
+    public Transform target;
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        if (mTexture == null)
+            return;
+        Gizmos.DrawGUITexture(new Rect(20, 20, 20f, 20f), mTexture);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(this.transform.position, new Vector3(2f, 2f, 2f));
+        
+        Gizmos.DrawIcon(this.transform.position + new Vector3(2, 2, 0), "node3", true);
+    }
+
+    /// <summary>
+	/// Callback to draw gizmos only if the object is selected.
+	/// </summary>
+	void OnDrawGizmosSelected()
+    {
+        if (target != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+	}
+}
