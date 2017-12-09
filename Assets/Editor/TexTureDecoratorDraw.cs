c890333d5c2cebd8d9e047e9d144b188b98b72e3
Lin--laptop <linyuan5856139@163.com>
2017-12-09 22:45:39 +0800

    DecoratorDraw

---
+++ Assets/Editor/TexTureDecoratorDraw.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Data))]
+public class TexTureDecoratorDraw : DecoratorDrawer
+{
+    
+    public override void OnGUI(Rect position)
+    {
+        Texture t = Resources.Load<Texture>("Node3");
+        EditorGUI.DrawTextureTransparent(position, t, ScaleMode.ScaleToFit);
+        
+    }
+}
---
+++ Assets/Script/7.DecoratorDraw/Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data  {
+
+    public float percent;
+
+    public int hp;
+
+    public string name;
+}
